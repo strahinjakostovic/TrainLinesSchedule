a5732bc3842d56dbc1259ba50d2cc45e6a01454c
strahinjakostovic <strahinjakostovic12@gmail.com>
2021-04-13 22:22:35 +0200

    Commenting out the code

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -77,7 +77,7 @@ $(document).ready(function(){
         for(var i=0;i<stops.length;i++) /* loop through the array */
         {
           /* add list item to the st variable, display name of the station, 
-          time of the bus arrival
+          train take off time
           and train number that is going through the station */
           st += "<li class='text-center text-uppercase'> <strong>Station name:   </strong> "+stops[i].name+"<strong>  Time:   </strong>"+stops[i].time+"<strong>  Train number:   </strong>"+stops[i].train_number+"</li><svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-arrow-down-square-fill' viewBox='0 0 16 16'><path d='M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm6.5 4.5v5.793l2.146-2.147a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-3-3a.5.5 0 1 1 .708-.708L7.5 10.293V4.5a.5.5 0 0 1 1 0z'/></svg>";
         }
